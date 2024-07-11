e889d20d06a442e315012258146910b0300402f3
aishwary <aishwary.agnihotri0512@gmail.com>
2024-07-12 02:51:21 +0530

    eslint setup and rules setup done

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    files: ["*.js"], // Adjust file patterns as needed
+    languageOptions: {
+      globals: {
+        process: "readonly"
+      },
+      parserOptions: {
+        ecmaVersion: 2021
+      }
+    },
+    plugins: {
+      node: {
+      }
+    },
+    rules: {
+      'no-console': 'off', // Allow console.log and others for development
+      'no-unused-vars': ['error', { 'args': 'none' }], // Enforce unused variables detection
+      'no-irregular-whitespace': 'error', // Disallow irregular whitespace
+      'prefer-const': 'error', // Prefer const over let when variable is not reassigned
+      'indent': ['error', 2], // Enforce 2 spaces indentation
+      'semi': ['error', 'always'], // Require semicolons at the end of statements
+    }
+  },
+];
