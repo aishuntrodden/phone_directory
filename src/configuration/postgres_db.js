12cdb016de5f0f85ea0320be6832feb6685565dd
aishwary <aishwary.agnihotri0512@gmail.com>
2024-07-13 17:07:25 +0530

    server db url set and tested queries with postman

--- src/configuration/postgres_db.js
+++ src/configuration/postgres_db.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DEPLOYED_DATABASE_URL, {
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true, // This will enable SSL connection
+      rejectUnauthorized: false, // This will allow you to connect to a server with a self-signed certificate
+    },
+  },
 });
 
 module.exports = {sequelize};
